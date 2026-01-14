8bf18ae18ae55c940c14e5ba25f15d63090196d0
Aitor Carrobles <aitorcarroblesmonge2@gmail.com>
2026-01-14 21:06:43 +0100

    Implementada lógica de listado y detalle de consolas (1:N)

--- app/Http/Controllers/ConsolaController.php
+++ app/Http/Controllers/ConsolaController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Consola;
 use Illuminate\Http\Request;
 
 class ConsolaController extends Controller
 {
-    //
+    //Muestra el listado de todas las consolas
+    public function index()
+    {
+        $consolas = Consola::all();
+
+        return view('consolas.index', compact('consolas'));
+    }
+
+    //Muestra los juegos de una consola específica
+    public function show($id)
+    {
+        $consola = Consola::with('videojuegos')->findOrFail($id);
+
+        return view('consolas.show', compact('consola'));
+    }
 }
