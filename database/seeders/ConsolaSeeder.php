599c7e378bb2ee31be25d8e65c3b95f04c0fe2a0
elvisentuchabe <vromerosaiz@gmail.com>
2026-01-15 23:44:23 +0100

    Cambio de datos en los seeders porque los juegos y consolas no eran retro |PENDIENTE DE ARREGLAR BDD (fabricante y anio publicacion)|

--- database/seeders/ConsolaSeeder.php
+++ database/seeders/ConsolaSeeder.php
@@ -10,16 +10,20 @@ class ConsolaSeeder extends Seeder
     public function run(): void
     {
         $consolas = [
+            ['nombre' => 'Atari 2600', 'fabricante' => 'Atari', 'anio_publicacion' => 1977, 'logo' => 'logos/atari2600.png'],
+            ['nombre' => 'Commodore 64', 'fabricante' => 'Commodore', 'anio_publicacion' => 1982, 'logo' => 'logos/c64.png'],
+            
             ['nombre' => 'NES', 'fabricante' => 'Nintendo', 'anio_publicacion' => 1983, 'logo' => 'logos/nes.png'],
-            ['nombre' => 'Super Nintendo', 'fabricante' => 'Nintendo', 'anio_publicacion' => 1990, 'logo' => 'logos/snes.png'],
-            ['nombre' => 'Nintendo 64', 'fabricante' => 'Nintendo', 'anio_publicacion' => 1996, 'logo' => 'logos/n64.png'],
-            ['nombre' => 'GameCube', 'fabricante' => 'Nintendo', 'anio_publicacion' => 2001, 'logo' => 'logos/gamecube.png'],
             ['nombre' => 'Master System', 'fabricante' => 'SEGA', 'anio_publicacion' => 1985, 'logo' => 'logos/mastersystem.png'],
+            
+            ['nombre' => 'Game Boy', 'fabricante' => 'Nintendo', 'anio_publicacion' => 1989, 'logo' => 'logos/gameboy.png'],
+            ['nombre' => 'Game Gear', 'fabricante' => 'SEGA', 'anio_publicacion' => 1990, 'logo' => 'logos/gamegear.png'],
+
+            ['nombre' => 'Super Nintendo', 'fabricante' => 'Nintendo', 'anio_publicacion' => 1990, 'logo' => 'logos/snes.png'],
             ['nombre' => 'Mega Drive', 'fabricante' => 'SEGA', 'anio_publicacion' => 1988, 'logo' => 'logos/megadrive.png'],
-            ['nombre' => 'Dreamcast', 'fabricante' => 'SEGA', 'anio_publicacion' => 1998, 'logo' => 'logos/dreamcast.png'],
+
             ['nombre' => 'PlayStation', 'fabricante' => 'Sony', 'anio_publicacion' => 1994, 'logo' => 'logos/ps1.png'],
-            ['nombre' => 'PlayStation 2', 'fabricante' => 'Sony', 'anio_publicacion' => 2000, 'logo' => 'logos/ps2.png'],
-            ['nombre' => 'Xbox', 'fabricante' => 'Microsoft', 'anio_publicacion' => 2001, 'logo' => 'logos/xbox.png'],
+            ['nombre' => 'Nintendo 64', 'fabricante' => 'Nintendo', 'anio_publicacion' => 1996, 'logo' => 'logos/n64.png'],
         ];
 
         foreach ($consolas as $c) {
--- database/seeders/JuegoSeeder.php
+++ database/seeders/JuegoSeeder.php
@@ -8,11 +8,12 @@ use App\Models\Consola;
 
 class JuegoSeeder extends Seeder
 {
-    public function run(): void {
+    public function run(): void
+    {
         $crearJuego = function ($nombreConsola, $titulo, $anio, $imagen, $desc) {
             $consola = Consola::where('nombre', $nombreConsola)->first();
             if ($consola) {
-                jUEGO::create([
+                Juego::create([
                     'titulo' => $titulo,
                     'anio_lanzamiento' => $anio,
                     'imagen' => $imagen,
@@ -22,34 +23,34 @@ class JuegoSeeder extends Seeder
             }
         };
 
-        $crearJuego('NES', 'Super Mario Bros', 1985, 'juegos/nes_mario.jpg', 'El juego que salvó la industria.');
-        $crearJuego('NES', 'The Legend of Zelda', 1986, 'juegos/nes_zelda.jpg', 'El inicio de una leyenda épica.');
+        $crearJuego('Atari 2600', 'Pac-Man', 1982, 'juegos/atari_pacman.jpg', 'El come-cocos original.');
+        $crearJuego('Atari 2600', 'Space Invaders', 1980, 'juegos/atari_space.jpg', 'Defiende la tierra de la invasión alienígena.');
 
-        $crearJuego('Super Nintendo', 'Super Mario World', 1990, 'juegos/snes_mario.jpg', 'Mario con capa y Yoshi.');
-        $crearJuego('Super Nintendo', 'Donkey Kong Country', 1994, 'juegos/snes_dk.jpg', 'Gráficos renderizados revolucionarios.');
+        $crearJuego('Commodore 64', 'Maniac Mansion', 1987, 'juegos/c64_maniac.jpg', 'La aventura gráfica que lo empezó todo.');
+        $crearJuego('Commodore 64', 'Bubble Bobble', 1987, 'juegos/c64_bubble.jpg', 'Dragones lanzando burbujas.');
 
-        $crearJuego('Nintendo 64', 'Super Mario 64', 1996, 'juegos/n64_mario.jpg', 'El paso al 3D definitivo.');
-        $crearJuego('Nintendo 64', 'The Legend of Zelda: Ocarina of Time', 1998, 'juegos/n64_zelda.jpg', 'Considerado el mejor juego de la historia.');
+        $crearJuego('NES', 'Super Mario Bros 3', 1988, 'juegos/nes_mario3.jpg', 'Considerado el mejor plataformas de 8 bits.');
+        $crearJuego('NES', 'The Legend of Zelda', 1986, 'juegos/nes_zelda.jpg', 'Es peligroso ir solo, toma esto.');
 
-        $crearJuego('GameCube', 'Super Smash Bros Melee', 2001, 'juegos/gc_smash.jpg', 'El juego de lucha competitivo por excelencia.');
-        $crearJuego('GameCube', 'Metroid Prime', 2002, 'juegos/gc_metroid.jpg', 'Samus Aran en primera persona.');
+        $crearJuego('Master System', 'Alex Kidd in Miracle World', 1986, 'juegos/ms_alex.jpg', 'Piedra, papel o tijera y hamburguesas.');
+        $crearJuego('Master System', 'Wonder Boy III', 1989, 'juegos/ms_wonder.jpg', 'La trampa del dragón.');
 
-        $crearJuego('Master System', 'Alex Kidd in Miracle World', 1986, 'juegos/ms_alex.jpg', 'Piedra, papel o tijera.');
-        $crearJuego('Master System', 'Sonic the Hedgehog', 1991, 'juegos/ms_sonic.jpg', 'La versión de 8 bits del clásico.');
+        $crearJuego('Game Boy', 'Tetris', 1989, 'juegos/gb_tetris.jpg', 'Desde Rusia con amor. El juego perfecto.');
+        $crearJuego('Game Boy', 'Pokémon Rojo', 1996, 'juegos/gb_pokemon.jpg', '¡Hazte con todos!');
 
-        $crearJuego('Mega Drive', 'Sonic the Hedgehog 2', 1992, 'juegos/md_sonic2.jpg', 'Introducción de Tails y el Spin Dash.');
-        $crearJuego('Mega Drive', 'Streets of Rage 2', 1992, 'juegos/md_streets.jpg', 'El mejor beat em up de la generación.');
+        $crearJuego('Game Gear', 'Sonic Chaos', 1993, 'juegos/gg_sonic.jpg', 'Sonic a todo color en tu mano.');
+        $crearJuego('Game Gear', 'Columns', 1990, 'juegos/gg_columns.jpg', 'La respuesta de Sega al Tetris.');
 
-        $crearJuego('Dreamcast', 'Sonic Adventure', 1998, 'juegos/dc_sonic.jpg', 'Sonic a la velocidad del sonido en 3D.');
-        $crearJuego('Dreamcast', 'Shenmue', 1999, 'juegos/dc_shenmue.jpg', 'Una historia de venganza y mundo abierto.');
+        $crearJuego('Super Nintendo', 'Chrono Trigger', 1995, 'juegos/snes_chrono.jpg', 'Viajes en el tiempo y un diseño inolvidable.');
+        $crearJuego('Super Nintendo', 'Street Fighter II Turbo', 1992, 'juegos/snes_sf2.jpg', 'El rey de la lucha.');
 
-        $crearJuego('PlayStation', 'Crash Bandicoot', 1996, 'juegos/ps1_crash.jpg', 'La mascota no oficial de Sony.');
-        $crearJuego('PlayStation', 'Metal Gear Solid', 1998, 'juegos/ps1_mgs.jpg', 'Espionaje táctico de Hideo Kojima.');
+        $crearJuego('Mega Drive', 'Sonic the Hedgehog 2', 1992, 'juegos/md_sonic2.jpg', 'Más rápido y con Tails.');
+        $crearJuego('Mega Drive', 'Streets of Rage 2', 1992, 'juegos/md_sor2.jpg', 'Música techno y peleas callejeras.');
 
-        $crearJuego('PlayStation 2', 'Grand Theft Auto: San Andreas', 2004, 'juegos/ps2_gta.jpg', 'Ah shit, here we go again.');
-        $crearJuego('PlayStation 2', 'God of War II', 2007, 'juegos/ps2_gow.jpg', 'Kratos desafiando al Olimpo.');
+        $crearJuego('PlayStation', 'Final Fantasy VII', 1997, 'juegos/ps1_ff7.jpg', 'La aventura de Cloud y Sephiroth.');
+        $crearJuego('PlayStation', 'Metal Gear Solid', 1998, 'juegos/ps1_mgs.jpg', 'Espionaje táctico de nueva generación.');
 
-        $crearJuego('Xbox', 'Halo: Combat Evolved', 2001, 'juegos/xbox_halo.jpg', 'El shooter que definió las consolas.');
-        $crearJuego('Xbox', 'Fable', 2004, 'juegos/xbox_fable.jpg', 'Por cada elección, una consecuencia.');
+        $crearJuego('Nintendo 64', 'Super Mario 64', 1996, 'juegos/n64_mario.jpg', 'La revolución del movimiento en 3D.');
+        $crearJuego('Nintendo 64', 'GoldenEye 007', 1997, 'juegos/n64_goldeneye.jpg', 'El shooter multijugador definitivo.');
     }
 }
